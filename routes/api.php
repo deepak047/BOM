de72ef3d5211f0b764ce8c56977f96bef9d1f858
deepak <deepak@fpletech.in>
2026-05-23 19:43:34 +0530

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/bom/upload', [BomUploadController::class, 'upload'])->middleware('can:upload-bom');
     Route::get('/bom/{id}', [BomUploadController::class, 'details'])->name('bom.details')->middleware('can:view-bom');
 
-    Route::get('/api/bom-line-items', [BomLineItemController::class, 'index'])->middleware('can:view-bom');
+    Route::get('/bom-line-items', [BomUploadController::class, 'index'])->middleware('can:view-bom');
     Route::get('/inventories', [InventoryController::class, 'index'])->middleware('can:view-bom');
     Route::get('/purchase-intents', [PurchaseIntentController::class, 'index'])->middleware('can:manage-purchase-intents');
     Route::get('/allocations', [MaterialAllocationController::class, 'index'])->middleware('can:view-allocations');
